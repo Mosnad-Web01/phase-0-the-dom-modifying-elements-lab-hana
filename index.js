04e24996eba070222bac40aae8b8ed3038dcf5e2
Hana Alhemyari <hana.saif.1978@gmail.com>
2024-08-15 13:16:55 +0300

    Update index.js
    .

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+
+
+
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
